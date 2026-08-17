c20475716225e8d362929d9b3d6d6d841a2759cf
Joao Pedro de Souza Santos <1094296@senacminas.edu.br>
2026-08-17 19:58:08 -0300

    DAO e MODEL pronto

---
+++ src/dao/UsuarioDao.java
@@ -0,0 +1,112 @@
+package dao;
+
+import controller.ConnectionMySql;
+import model.UsuarioModel;
+import java.sql.Connection;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+public class UsuarioDao {
+    private Connection connection;
+
+    public UsuarioDao(Connection connection) {
+        this.connection = new ConnectionMySql().getConnection();
+    }
+    
+       public void adicionar(UsuarioModel usuario) {
+        String sql = "INSERT INTO usuário(nome,cpf,email,telefone,nascimento)VALUES(?,?,?,?,?)";
+        
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            
+            ps.setString(1, usuario.getNome());
+            ps.setString(2, usuario.getPlataforma());
+            ps.setDouble(3, usuario.getValor());
+
+            ps.execute();
+            ps.close();
+            
+            
+        } catch (SQLException e) {
+        JOptionPane.showMessageDialog(null, "Erro ao cadastrar! ERRO DAO "
+                   + "Classe UsuarioDAO");
+            throw new RuntimeException(e);
+        }
+    }
+       
+        public List<UsuarioModel> leitura(){
+        connection = new ConnectionMySql().getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        List<UsuarioModel> usuarioArray = new ArrayList<>();
+        
+        try {
+            ps = connection.prepareStatement(
+            "SELECT * FROM jogo");
+            
+            rs = ps.executeQuery();
+            
+            while (rs.next()) {                
+                UsuarioModel u = new UsuarioModel();
+                
+                u.setIdJogo     (rs.getInt      ("idJogo"));
+                u.setNome       (rs.getString   ("nome"));
+                u.setPlataforma (rs.getString   ("plataforma"));
+                u.setValor      (rs.getDouble   ("preco"));
+
+                
+                usuarioArray.add(u);
+            }
+            
+           JOptionPane.showMessageDialog(null, "Lista DAO Funcionou");
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null,"Erro Listar DAO");
+            throw new RuntimeException(e);
+        }
+        return usuarioArray;
+    }
+        
+           public void deletar(UsuarioModel usuario){
+                String sql = "DELETE FROM jogo WHERE idJogo = ?";
+        
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+                    
+            ps.setInt(1, usuario.getIdJogo());
+            
+            ps.execute();
+            JOptionPane.showMessageDialog(null, "Deu Bom");
+            ps.close();
+            
+        } catch (SQLException e) {
+             JOptionPane.showMessageDialog(null, "Deu Ruim - Classe DAO");
+             throw new RuntimeException(e);
+        }
+    }
+    
+                public void atualizar(UsuarioModel usuario) {
+        String sql = "UPDATE jogo SET nome = ?, plataforma = ?, preco ?";
+        
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            
+            ps.setString(1, usuario.getNome());
+            ps.setString(2, usuario.getPlataforma());
+            ps.setDouble(3, usuario.getValor());
+            
+            ps.execute();
+            ps.close();
+            
+            
+        } catch (SQLException e) {
+           JOptionPane.showMessageDialog(null, "Erro ao cadastrar! ERRO DAO "
+                    + "Classe UsuarioDAO");
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/model/UsuarioModel.java
+++ src/model/UsuarioModel.java
@@ -2,6 +2,41 @@ package model;
 
 
 public class UsuarioModel {
-    
+    private int idJogo;
+    private String nome;
+    private String plataforma;
+    private double valor;
+
+    public int getIdJogo() {
+        return idJogo;
+    }
+
+    public void setIdJogo(int idJogo) {
+        this.idJogo = idJogo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPlataforma() {
+        return plataforma;
+    }
+
+    public void setPlataforma(String plataforma) {
+        this.plataforma = plataforma;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
     
 }
